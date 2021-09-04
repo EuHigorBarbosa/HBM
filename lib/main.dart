c573782b2d23d848dae382b9c66f0d463da4b165
Higor Barbosa <higorbarbosa@MacBook-Pro-de-Higor.local>
2021-09-04 12:26:56 -0300

    Estado Antes classe Transation

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 main() => runApp(ExpensesApp());
 
-//Estou criando um widget MaterialApp e um Center
+//Estou criando um widget MaterialApp e um scafold
 class ExpensesApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -15,6 +15,36 @@ class ExpensesApp extends StatelessWidget {
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Versão Inicial'));
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Despesas Pessoais'),
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            Container(
+              color: Colors.pink,
+              //padding: EdgeInsets.all(10.0),
+              //margin: EdgeInsets.all(100.0),
+              //alignment: Alignment.center,
+              //width: double.infinity,
+              child: Card(
+                child: Text(
+                  'Grafico',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                ),
+
+                elevation: 5,
+                //margin: EdgeInsets.all(4.0),
+                color: Colors.blue,
+                //borderOnForeground: false,
+              ),
+            ),
+            Card(
+              child: Text('Lista de Transações'),
+            )
+          ],
+        ));
   }
 }
