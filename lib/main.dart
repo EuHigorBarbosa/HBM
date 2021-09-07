d0727109c2fa8661a4af2ed27829c041c87af1d7
Higor Barbosa <higorbarbosa@MacBook-Pro-de-Higor.local>
2021-09-07 07:55:04 -0300

    Botoes de Icones adicionados mas vazios

--- lib/main.dart
+++ lib/main.dart
@@ -19,35 +19,44 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Despesas Pessoais'),
-        ),
+      appBar: AppBar(
+        title: Text('Despesas Pessoais'),
+        actions: <Widget>[IconButton(onPressed: () {}, icon: Icon(Icons.add))],
+      ),
+
 //? ======================= COLUNA DO GRAFICO ===============
-        body: ListView(
-            //crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Container(
-                color: Colors.pink,
-                //padding: EdgeInsets.all(10.0),
-                // margin: EdgeInsets.symmetric(
-                //   horizontal: 15,
-                //   vertical: 10,
-                // ),
-                //alignment: Alignment.center,
-                //width: double.infinity,
-                child: Card(
-                  child: Text(
-                    'Grafico2',
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-                  ),
-                  elevation: 5,
-                  //margin: EdgeInsets.all(4.0),
-                  color: Colors.blue,
-                  //borderOnForeground: false,
-                ),
+      body: ListView(
+        //crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            color: Colors.pink,
+            //padding: EdgeInsets.all(10.0),
+            // margin: EdgeInsets.symmetric(
+            //   horizontal: 15,
+            //   vertical: 10,
+            // ),
+            //alignment: Alignment.center,
+            //width: double.infinity,
+            child: Card(
+              child: Text(
+                'Grafico2',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
               ),
-              //* =============== Eixo PRINCIPAL DOS CARDS dentro de Column ============
-              TransactionUser(),
-            ]));
+              elevation: 5,
+              //margin: EdgeInsets.all(4.0),
+              color: Colors.blue,
+              //borderOnForeground: false,
+            ),
+          ),
+          //* =============== Eixo PRINCIPAL DOS CARDS dentro de Column ============
+          TransactionUser(),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
   }
 }
